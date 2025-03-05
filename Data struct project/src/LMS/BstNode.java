60a4a5770d8717b3fa6a10304c5f76c4f384d032
MasonKadedra <162503087+MasonKadedra@users.noreply.github.com>
2025-03-05 11:25:53 -0500

    Create BstNode.java

---
+++ Data struct project/src/LMS/BstNode.java
@@ -0,0 +1,13 @@
+package LMS;
+
+public class BstNode {
+  Books book;
+  BstNode left, right;
+
+  public BstNode(Books book) {
+    this.book = book;
+    left = null;
+    right = null;
+  }
+}
+
