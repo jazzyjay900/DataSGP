145a843c04ff3317d1f0977147c8412cfd574f7c
MasonKadedra <162503087+MasonKadedra@users.noreply.github.com>
2025-03-05 10:20:34 -0500

    Update LinkList.java

--- Data struct project/src/LMS/LinkList.java
+++ Data struct project/src/LMS/LinkList.java
@@ -11,4 +11,32 @@ public class LinkList {
   public LinkList(Node h) {
     head = h;
   }
+  
+  public Node getHead() {
+    return head;
+  }
+  
+  public void setHead(Node head) {
+    this.head = head;
+  }
 
+  public void addBooks(Books dataToAdd) {
+    Node temp;
+    temp = new Node();
+    if(temp != null) {
+      temp.setdata(dataToAdd);
+      temp.setNextNode(null);
+
+      if(head == null) {
+        head = temp;
+      }
+      else {
+        temp.setNextNode(head);
+        head = temp;
+      }
+    }
+    else {
+      System.err.println("List is full! Out of memory");
+    }
+  }  
+}
