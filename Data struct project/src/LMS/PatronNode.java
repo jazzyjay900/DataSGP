a6cd188e28524849fa685621be7b96f9186e7460
MasonKadedra <162503087+MasonKadedra@users.noreply.github.com>
2025-03-12 18:42:50 -0500

    Update PatronNode.java

--- Data struct project/src/LMS/PatronNode.java
+++ Data struct project/src/LMS/PatronNode.java
@@ -1,1 +1,29 @@
+package LMS;
 
+public class PatronNode() {
+  
+  private Patron data;
+  private PatronNode node;
+
+  public PatronNode() {
+
+    data = new Patron();
+    node = null;
+  }
+
+  public PatronNode(Patron Data, PatronNode Node) {
+
+    data = new Patron(Data);
+    node = Node;
+  }
+
+  public PatronNode(Patron Data) {
+
+    data = new Patron(Data);
+    node = null;
+  }
+
+  public PatronNode(
+
+
+}
